c5318847418185b34c289a63039764f185bfecb7
Siddhika <siddhikamathur279@gmail.com>
2024-04-27 18:57:48 +0530

    hello

---
+++ SET1/hexToBase64.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+float f1 = 35e3;
+float d1 = 12e3;
+
+printf("%f\n", f1);
+printf("%f\n", d1);
+printf("%f", d1/f1);
+return 0;
+}
