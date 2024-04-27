36d9e65dda0b9cd0eae9ec3da1457c4308d28e81
Siddhika <siddhikamathur279@gmail.com>
2024-04-28 00:19:36 +0530

    yes

--- SET1/hexToBase64.c
+++ SET1/hexToBase64.c
@@ -1,13 +1,64 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
+#include <string.h>
+
+// Function to convert hexadecimal character to its integer value
+int hexCharToInt(char c) {
+    if (c >= '0' && c <= '9')
+        return c - '0';
+    else if (c >= 'a' && c <= 'f')
+        return c - 'a' + 10;
+    else if (c >= 'A' && c <= 'F')
+        return c - 'A' + 10;
+    else
+        return -1;  // Invalid character
+}
+
+// Function to convert a hexadecimal string to binary string
+void hexToBinary(const char* hexString, char* binaryString) {
+    int i, j = 0;
+    for (i = 0; i < strlen(hexString); i++) {
+        int val = hexCharToInt(hexString[i]);
+        binaryString[j++] = (val & 8) ? '1' : '0';
+        binaryString[j++] = (val & 4) ? '1' : '0';
+        binaryString[j++] = (val & 2) ? '1' : '0';
+        binaryString[j++] = (val & 1) ? '1' : '0';
+    }
+    binaryString[j] = '\0';
+}
+
+// Function to convert a binary string to base64
+void binaryToBase64(const char* binaryString, char* base64String) {
+    const char* base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    int i, j = 0;
+    for (i = 0; i < strlen(binaryString); i += 6) {
+        int val = 0;
+        int k;
+        for (k = 0; k < 6; k++) {
+            val <<= 1;
+            if (binaryString[i + k] == '1')
+                val |= 1;
+        }
+        base64String[j++] = base64chars[val];
+    }
+
+    // Padding
+    while (j % 4 != 0) {
+        base64String[j++] = '=';
+    }
+    base64String[j] = '\0';
+}
 
 int main() {
-float f1 = 35e3;
-float d1 = 12e3;
+    const char* hexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    char binaryString[1000];
+    char base64String[1000];
 
-printf("%f\n", f1);
-printf("%f\n", d1);
-printf("%f", d1/f1);
-return 0;
+    hexToBinary(hexString, binaryString);
+    binaryToBase64(binaryString, base64String);
+
+    printf("Base64: %s\n", base64String);
+
+    return 0;
 }
